ac71dc1eb99dcf2dfacd117a7066d5af1471bdbe
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2024-09-19 18:12:00 -0300

    Criei script03 em aula06

---
+++ aula06/script03.js
@@ -0,0 +1,5 @@
+var n1 = Number.parseInt(window.prompt('Digite um número: '))
+var n2 = Number.parseInt(window.prompt('Digite outro número: '))
+var s = n1 + n2
+window.alert('Soma dos valores é igual a: '+s)
+// Atualmente usasse somente Number e JS define se o numro é inteiro ou flutuante
